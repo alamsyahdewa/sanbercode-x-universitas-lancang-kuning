2a28cfdb9c742ade44e6da0115762b95535b20b0
alamsyahdewa <alamsyahdewa08@gmail.com>
2024-06-02 04:14:42 +0700

    laravel empat belas

---
+++ cerdas/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+Halaman Utama
+@endsection
+    
+@section('content')
+    <ul>
+        <li><a href="/daftar">Ke biodata</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+    <ul>
+    @endsection
