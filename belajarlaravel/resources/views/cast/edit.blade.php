cd40586540ca8e514d70f8e5eba7cd32f281ef34
alamsyahdewa <alamsyahdewa08@gmail.com>
2024-06-02 04:01:17 +0700

    tugas 14

---
+++ belajarlaravel/resources/views/cast/edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('title')
+Halaman Edit Cast
+@endsection
+
+@section('content')
+
+<form method="POST" action="/cast/{{$cast->id}}">
+    {{--validation--}}
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    {{--Form Input--}}
+    @csrf
+    @method("put")
+    <div class="form-group">
+      <label >Cast Name</label>
+      <input type="text" value ="{{$cast->name}}" class="form-control" name="name">
+    <div class="form-group">
+        <label >Cast Umur</label>
+        <input type="number" value ='{{$cast->umur}}' class="form-control" name="umur">
+    </div>
+    <div class="form-group">
+      <label >Cast Bio</label>
+     <textarea name="bio" class="form-control" cols="30" rows="10">{{$cast->bio}}</textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+@endsection
